8e0ee3f7e61124ef9c923643d1b064d23995234e
Ashley Pean <pean.ashley@gmail.com>
2020-12-07 10:36:45 -0500

    Added toggle function to change background color with dark mode

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import './App.css';
-import React from 'react'
+import React, {useContext} from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage/Homepage'
 import SearchResult from './components/SearchResult/SearchResult'
 import NotFound from './components/404/NotFound'
-import {ThemeProvider}  from './utils/ThemeContext'
+import { DarkModeContext } from './utils/DarkModeHook';
+import styled from 'styled-components'
 
+const AppComponent = styled.div`
+  background-color: ${props => props.darkMode? "#fafafa": "#202c37"};
+  height: 100%;
+`
 
 export default function App() {
+  const {darkMode} = useContext(DarkModeContext)
   return (
-    <ThemeProvider>
-      {value => (
-        <div className="App">
+      <AppComponent className="App" darkMode={darkMode}>
         <Router>
           <Switch>
             <Route exact path="/" component={Homepage}/>
-            <Route path="/search/:country" component={SearchResult}/>
-            <Route component={NotFound}/>
+            <Route path="/search:country" component={SearchResult}/>
+            <Route component={NotFound} />
           </Switch>
         </Router>
-      </div>
-      )}
-    </ThemeProvider>
-  );
+    </AppComponent>
+  )
 }
 
