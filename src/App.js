9b6a0641ad40eeb50283c12b3a2bd2ef957f13a0
Ashley Pean <pean.ashley@gmail.com>
2020-12-07 12:33:39 -0500

    removed app.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React, {useContext} from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage/Homepage'
