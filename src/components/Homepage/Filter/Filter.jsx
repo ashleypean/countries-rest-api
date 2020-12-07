9020fe0731b44c32b033bb1d0372468abfa3fee1
Ashley Pean <pean.ashley@gmail.com>
2020-12-07 09:17:14 -0500

    homepage components

---
+++ src/components/Homepage/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Dropdown = styled.div`
+
+`
+
+const DropdownList = styled.ul`
+
+`
+
+const DropdownListItems = styled.li`
+
+`
+
+export default function Filter() {
+  return (
+    <Dropdown>
+      
+    </Dropdown>
+  )
+}
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Homepage.css'
+import Header from '../Header/Header'
+import Filter from './Filter/Filter'
+import Search from './Search/Search'
+import Footer from '../Footer/Footer'
 
 export default function Homepage() {
+
+
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
