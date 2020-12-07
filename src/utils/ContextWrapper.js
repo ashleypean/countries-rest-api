098c1409b2aca1ce8796c41d4c5a9fa1e4ab5466
Ashley Pean <pean.ashley@gmail.com>
2020-12-07 10:37:23 -0500

    Dark Mode Hook and tooglle

---
+++ src/utils/ContextWrapper.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { DarkModeContext } from './DarkModeHook'
+
+
+export default function ContextWrapper(props) {
+  const [darkMode, setDarkMode] = useState(false)
+
+  function toggleDarkMode() {
+    setDarkMode(darkMode => !darkMode)
+  }
+
+  return (
+    <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
+      {props.children}
+    </DarkModeContext.Provider>
+  )
+}
+
