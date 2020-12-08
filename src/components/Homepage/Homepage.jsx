6dcee1aeaf105ba2ce6b6f7cffc20c4b3d640e59
Ashley Pean <pean.ashley@gmail.com>
2020-12-08 14:13:25 -0500

    Moved the country API call state to the homepage so the filter component can update it

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './Homepage.css'
 import Header from '../Header/Header'
 import Filter from './Filter/Filter'
 import SearchBar from './SearchBar/SearchBar'
 import Card from './Card/Card'
 import Footer from '../Footer/Footer'
+import axios from 'axios'
 
 export default function Homepage() {
+
+  const [countryList, setCountryList] = useState([{
+    flag: '', 
+    name: '', 
+    population: 0, 
+    region: '', 
+    capital: '', 
+  }])
+
+  //Fetch countries list from the third-party API
+  useEffect(  () => {
+    async function getData(){
+      const res = await axios.get('https://restcountries.eu/rest/v2/all')
+
+      let data = res.data
+
+      setCountryList(
+        data.map(country => {
+          return {
+          flag: country.flag, 
+          name: country.name, 
+          population: country.population.toLocaleString(), 
+          region: country.region, 
+          capital: country.capital
+          }
+        })
+      )
+
+    }
+    getData()
+  }, [])
   return (
     <div>
       <Header />
       <SearchBar />
-      <Filter />
-      <Card />
+      <Filter setCountryList={setCountryList}/>
+      <Card countryList={countryList}/>
     </div>
   )
 }
