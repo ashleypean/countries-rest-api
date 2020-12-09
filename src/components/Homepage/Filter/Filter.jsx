e4bf5e6601db34575cd3fa2bf439830edca077cb
Ashley Pean <pean.ashley@gmail.com>
2020-12-08 19:06:19 -0500

    Adjusted max-width: of filter button

--- src/components/Homepage/Filter/Filter.jsx
+++ src/components/Homepage/Filter/Filter.jsx
@@ -5,34 +5,46 @@ import { CaretDown } from '@styled-icons/boxicons-regular/CaretDown'
 import axios from 'axios'
 
 const Dropdown = styled.div`
-margin: 1rem 1rem 0;
-width: 50%;
-height: 20%;
-padding: 1rem;
-background: ${props => props.darkMode? "white": "#2B3945"};
-color: ${props => props.darkMode? "black":"white"};
+  margin: 1rem 1rem 0;
+  width: 50%;
+  max-width: 150px;
+  height: 20%;
+  padding: 1rem;
+  background: ${props => props.darkMode? "white": "#2B3945"};
+  color: ${props => props.darkMode? "black":"white"};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const DropdownIcon = styled(CaretDown)`
+  display: inline;
+  height: 1rem; 
+  width: 1rem;
+  margin: 0;
 `
 
 const DropdownList = styled.ul`
-display: 'block';
-margin: .25rem 1rem;
-width: calc(50% + 2rem);
-padding: 0;
-background: ${props => props.darkMode? "white": "#2B3945"};
-list-style: none;
-z-index: 2;
+  display: 'block';
+  margin: .25rem 1rem;
+  width: calc(50% + 2rem);
+  max-width: calc(150px + 2rem);
+  padding: 0;
+  background: ${props => props.darkMode? "white": "#2B3945"};
+  list-style: none;
+  z-index: 2;
 
-&.hidden {
-  display: none;
-}
+  &.hidden {
+    display: none;
+  }
 `
 
 const DropdownListItem = styled.li`
-color: ${props => props.darkMode? "black":"white"};
-padding: .5rem 2rem;
+  color: ${props => props.darkMode? "black":"white"};
+  padding: .5rem 2rem;
 
-&:hover {
-  background: ${props => props.darkMode? "gray": "#202c37"}
+  &:hover {
+    background: ${props => props.darkMode? "gray": "#202c37"}
 }
 `
 
@@ -79,6 +91,7 @@ export default function Filter(props) {
     <>
       <Dropdown darkMode={darkMode} onClick={toggleDropdown}> 
         Filter by Region 
+        <DropdownIcon />
       </Dropdown>
       <DropdownList darkMode={darkMode} hidden={true}>
         {listItems.map((item, index) => (
